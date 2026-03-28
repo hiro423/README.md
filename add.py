1783a193023b313e7e61735ac631ea853f534785
Eric <ericpaiva70@gmail.com>
2026-03-28 14:55:17 -0300

    https://github.com/hiro423/README.md.git

--- add.py
+++ add.py
@@ -1,16 +1,20 @@
-        import pandas as pd
-        import plotly.express as px
-        import streamlit as st
-        
-        car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
-        hist_button = st.button('Criar histograma') # criar um botão
-        
-        if hist_button: # se o botão for clicado
-            # escrever uma mensagem
-            st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-            
-            # criar um histograma
-            fig = px.histogram(car_data, x="odometer")
-        
-            # exibir um gráfico Plotly interativo
-            st.plotly_chart(fig, use_container_width=True)
+import pandas as pd
+import plotly.express as px
+import streamlit as st # type: ignore
+
+# Lendo os dados
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Criar um botão
+hist_button = st.button('Criar histograma')
+
+# Se o botão for clicado
+if hist_button:
+    # Escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+
+    # Criar um histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # Exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
